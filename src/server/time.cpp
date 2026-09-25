3e6a076c48dac75c7e07bf3dea47580bc4de613f
gerardangelovega <vegagerard1130@gmail.com>
2026-09-25 18:23:11 +0800

    fix(time): corrected incorrect ns to ms conversion which caused inconsistent timeouts and expirations

--- src/server/time.cpp
+++ src/server/time.cpp
@@ -14,7 +14,7 @@
 uint64_t get_monotonic_ms() {
     struct timespec tv = {0, 0};
     clock_gettime(CLOCK_MONOTONIC, &tv);
-    return uint64_t(tv.tv_sec) * 1000 + tv.tv_nsec / 1000 / 100;
+    return uint64_t(tv.tv_sec) * 1000 + tv.tv_nsec / 1000 / 1000;
 }
 
 int32_t next_timer_ms() {
